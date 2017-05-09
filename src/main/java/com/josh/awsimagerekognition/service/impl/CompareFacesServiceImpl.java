fd3013f884857055b2871e0bf839a667da247d1b
Jyothsna Annavarapu <jyothsnayekkanti@gmail.com>
2017-05-09 02:15:05 +0100

    Compare Faces given S3 Bucket Images/file path

--- src/main/java/com/josh/awsimagerekognition/service/impl/CompareFacesServiceImpl.java
+++ src/main/java/com/josh/awsimagerekognition/service/impl/CompareFacesServiceImpl.java
@@ -1,7 +1,10 @@
 package com.josh.awsimagerekognition.service.impl;
 
 import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.model.CompareFacesRequest;
 import com.amazonaws.services.rekognition.model.CompareFacesResult;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.S3Object;
 import com.josh.awsimagerekognition.service.AmazonRekognitionClientService;
 import com.josh.awsimagerekognition.service.CompareFacesService;
 
@@ -17,7 +20,16 @@ public class CompareFacesServiceImpl implements CompareFacesService {
 
     @Override
     public CompareFacesResult compareFacesGivenS3Images(String sourceBucketName, String sourceFilePath, String targetBucketName, String targetFilePath) {
-        return null;
+        Image source = getImageUtil(sourceBucketName, sourceFilePath);
+        Image target = getImageUtil(targetBucketName, targetFilePath);
+        Float similarityThreshold = 70F;
+
+        CompareFacesRequest compareFacesRequest = new CompareFacesRequest()
+                .withSourceImage(source)
+                .withTargetImage(target)
+                .withSimilarityThreshold(similarityThreshold);
+
+        return amazonRekognition.compareFaces(compareFacesRequest);
     }
 
     @Override
@@ -29,4 +41,12 @@ public class CompareFacesServiceImpl implements CompareFacesService {
     public CompareFacesResult compareFacesGivenImages(ByteBuffer sourceImage, ByteBuffer targetImage) {
         return null;
     }
+
+    private Image getImageUtil(String bucket, String key) {
+        return new Image()
+                .withS3Object(new S3Object()
+                        .withBucket(bucket)
+                        .withName(key));
+    }
+
 }
